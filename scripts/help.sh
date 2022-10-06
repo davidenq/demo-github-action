3c947f152b723e34fa438596e8b12d94b566e82b
David Nuñez <david.nunez.dev@gmail.com>
2022-10-06 04:24:59 -0500

    feat: support scripts for multiple tasks  with make as entry point

---
+++ scripts/help.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function action {
+
+  section 'Show environment variable'
+  quote $BRANCH_NAME
+
+  section 'Summary:'
+  summary 'this is just and example'
+
+  section 'Command:'
+  command 'make help [Target]'
+
+  section 'Targets'
+  options '
+  - action
+  '
+
+  section 'Example'
+  options '
+  - make help action
+  '
+}
